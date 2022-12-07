612fa3dd8e0e811f45fe1044810d844f8fb1a8f3
PerseP <persep@gmail.com>
2022-12-07 16:39:23 +0100

    Changes to upload to Tb

--- upload_tb.sh
+++ upload_tb.sh
@@ -1,25 +1,20 @@
 #!/bin/bash -e
 
+data_dir="data"
+
+today=$(date +'%Y-%m-%d')
+file=$today-products.json
+
+echo "Converting $data_dir/$file to temp.csv"
+jq -r '.[] | [.[]] | @csv' $data_dir/$file > temp.csv
+
 echo "Uploading product to Tb"
 
-#data_dir="data"
-#
-#today=$(date +'%Y-%m-%d')
-#file=$today-products.json
-#
-#echo "Converting $data_dir/$file to csv"
-#jq -r '.[] | [.[]] | @csv' $data_dir/$file > temp.json
-#
-#echo "Uploading product to Tb"
-#
-#
-#rm ./temp.json
-
-
 curl \
-  -H 'Authorization: Bearer ${TB_TOKEN}' \
-  -X POST 'https://api.tinybird.co/v0/datasources?format=csv&name=products&mode=append' \
+  -H "Authorization: Bearer ${TB_TOKEN}" \
+  -X POST "https://api.tinybird.co/v0/datasources?format=csv&name=products&mode=append" \
   -F "csv=@temp.csv"
 
+rm ./temp.csv
 
 exit 0
