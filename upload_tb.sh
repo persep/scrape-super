a572a570c708a654f9cf1faef6d2703d3d4cc0f3
PerseP <persep@gmail.com>
2022-12-07 14:20:37 +0100

    Add test to tb

---
+++ upload_tb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+echo "Uploading product to Tb"
+
+#data_dir="data"
+#
+#today=$(date +'%Y-%m-%d')
+#file=$today-products.json
+#
+#echo "Converting $data_dir/$file to csv"
+#jq -r '.[] | [.[]] | @csv' $data_dir/$file > temp.json
+#
+#echo "Uploading product to Tb"
+#
+#
+#rm ./temp.json
+
+curl \
+  -H 'Authorization: Bearer $TB_TOKEN' \
+  -X POST 'https://api.tinybird.co/v0/datasources?format=csv&name=products&mode=append' \
+  -F "csv=@temp.csv"
+
+
+exit 0
