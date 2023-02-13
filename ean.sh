9a7aac171a4e088e1e5dcf743b630a689ce7b2cb
PerseP <persep@gmail.com>
2023-02-13 14:18:29 +0100

    Use a lookup table to speed ean jsons

--- ean.sh
+++ ean.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -e
 
+declare -A lookup
+
+while read id; do
+    lookup[$id]=$id
+done < <(jq -r '.[].id' data-ean/products-ean.json)
+
 today=$(date +'%Y-%m-%d')
 
 n=1
 
-echo "Downloading new products ean"
+echo "Cheking for new products ean"
 while read id; do
     file="product-$id.json"
     url="https://tienda.mercadona.es/api/products/$id/?lang=es&wh=alc1"
-    if [ ! -f "data-ean/data-raw/$file" ]; then
+    if [ -z lookup[$id] ]; then
         echo "$n. $id"
         curl --fail --show-error --silent --compressed $url --output "data-ean/data-raw/$file"
         ((n++))
