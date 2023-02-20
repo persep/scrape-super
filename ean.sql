5b46455527b4a3e2b444ad8eb07cf8764e55e1e3
PerseP <persep@gmail.com>
2023-02-20 10:48:53 +0100

    Fixed ean order in inserting to db

--- ean.sql
+++ ean.sql
@@ -1,12 +1,13 @@
-CREATE TABLE IF NOT EXISTS ean(id VARCHAR UNIQUE, ean VARCHAR, brand VARCHAR, url VARCHAR, name VARCHAR, description VARCHAR, category VARCHAR);
+CREATE TABLE IF NOT EXISTS ean(id VARCHAR UNIQUE, ean VARCHAR, brand VARCHAR, url VARCHAR, 
+  name VARCHAR, description VARCHAR, category VARCHAR);
 
 INSERT OR IGNORE into ean(id, ean, brand, url, name, description, category)
 select
   json ->> '$.id',
   json ->> '$.ean',
-  json ->> '$.name',
   json ->> '$.brand',
   json ->> '$.url',
+  json ->> '$.name',
   json ->> '$.description',
   json ->> '$.category'
 from read_ndjson_objects('temp-ean.ndjson');
